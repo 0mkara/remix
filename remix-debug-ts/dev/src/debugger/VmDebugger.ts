7110893546ef61f10fe1179b79c952d7b26a73e1
Ayan Banerjee <ayanb1999@gmail.com>
2019-04-22 21:59:56 +0530

    debugger #10

--- remix-debug-ts/dev/src/debugger/VmDebugger.ts
+++ remix-debug-ts/dev/src/debugger/VmDebugger.ts
@@ -70,18 +70,22 @@ export default class VmDebuggerLogic {
 
   listenToEvents(): void {
     const self = this;
-    this.debugger.event.register("traceUnloaded", function() {
+    this.debugger.event.register("traceUnloaded", function(): void {
       self.event.trigger("traceUnloaded");
     });
 
-    this.debugger.event.register("newTraceLoaded", function() {
+    this.debugger.event.register("newTraceLoaded", function(): void {
       self.event.trigger("newTraceLoaded");
     });
   }
 
   listenToCodeManagerEvents(): void {
     const self = this;
-    this._codeManager.event.register("changed", function(code, address, index) {
+    this._codeManager.event.register("changed", function(
+      code: any,
+      address: any,
+      index: number
+    ): void {
       self.event.trigger("codeManagerChanged", [code, address, index]);
     });
   }
@@ -89,7 +93,7 @@ export default class VmDebuggerLogic {
   listenToTraceManagerEvents(): void {
     const self = this;
 
-    this.event.register("indexChanged", this, function(index: number) {
+    this.event.register("indexChanged", this, function(index: number): void {
       if (index < 0) return;
       if (self.stepManager.currentStepIndex !== index) return;
 
@@ -110,7 +114,7 @@ export default class VmDebuggerLogic {
       self._traceManager.getMemoryAt(index, function(
         error: Error,
         memory: any
-      ) {
+      ): void {
         if (error) {
           // console.log(error)
           self.event.trigger("traceManagerMemoryUpdate", [{}]);
@@ -121,7 +125,10 @@ export default class VmDebuggerLogic {
         }
       });
 
-      self._traceManager.getCallStackAt(index, function(error, callstack) {
+      self._traceManager.getCallStackAt(index, function(
+        error: Error,
+        callstack: any
+      ): void {
         if (error) {
           // console.log(error)
           self.event.trigger("traceManagerCallStackUpdate", [{}]);
@@ -130,7 +137,10 @@ export default class VmDebuggerLogic {
         }
       });
 
-      self._traceManager.getStackAt(index, function(error, callstack) {
+      self._traceManager.getStackAt(index, function(
+        error: Error,
+        callstack: any
+      ): void {
         if (error) {
           // console.log(error)
           self.event.trigger("traceManagerStackUpdate", [{}]);
@@ -139,48 +149,65 @@ export default class VmDebuggerLogic {
         }
       });
 
-      self._traceManager.getCurrentCalledAddressAt(index, (error, address) => {
-        if (error) return;
-        if (!self.storageResolver) return;
+      self._traceManager.getCurrentCalledAddressAt(
+        index,
+        (error: Error, address: any): void => {
+          if (error) return;
+          if (!self.storageResolver) return;
 
-        var storageViewer = new StorageViewer(
-          {
-            stepIndex: self.stepManager.currentStepIndex,
-            tx: self.tx,
-            address: address
-          },
-          self.storageResolver,
-          self._traceManager
-        );
+          var storageViewer = new StorageViewer(
+            {
+              stepIndex: self.stepManager.currentStepIndex,
+              tx: self.tx,
+              address: address
+            },
+            self.storageResolver,
+            self._traceManager
+          );
 
-        storageViewer.storageRange((error, storage) => {
-          if (error) {
-            // console.log(error)
-            self.event.trigger("traceManagerStorageUpdate", [{}]);
-          } else if (self.stepManager.currentStepIndex === index) {
-            var header = storageViewer.isComplete(address)
-              ? "completely loaded"
-              : "partially loaded...";
-            self.event.trigger("traceManagerStorageUpdate", [storage, header]);
-          }
-        });
-      });
+          storageViewer.storageRange(
+            (error: Error, storage: any): void => {
+              if (error) {
+                // console.log(error)
+                self.event.trigger("traceManagerStorageUpdate", [{}]);
+              } else if (self.stepManager.currentStepIndex === index) {
+                var header = storageViewer.isComplete(address)
+                  ? "completely loaded"
+                  : "partially loaded...";
+                self.event.trigger("traceManagerStorageUpdate", [
+                  storage,
+                  header
+                ]);
+              }
+            }
+          );
+        }
+      );
 
-      self._traceManager.getCurrentStep(index, function(error, step) {
+      self._traceManager.getCurrentStep(index, function(
+        error: Error,
+        step: any
+      ): void {
         self.event.trigger("traceCurrentStepUpdate", [error, step]);
       });
 
-      self._traceManager.getMemExpand(index, function(error, addmem) {
+      self._traceManager.getMemExpand(index, function(
+        error: Error,
+        addmem: any
+      ): void {
         self.event.trigger("traceMemExpandUpdate", [error, addmem]);
       });
 
-      self._traceManager.getStepCost(index, function(error, gas) {
+      self._traceManager.getStepCost(index, function(
+        error: Error,
+        gas: any
+      ): void {
         self.event.trigger("traceStepCostUpdate", [error, gas]);
       });
 
       self._traceManager.getCurrentCalledAddressAt(index, function(
-        error,
-        address
+        error: Error,
+        address: any
       ) {
         self.event.trigger("traceCurrentCalledAddressAtUpdate", [
           error,
@@ -188,11 +215,17 @@ export default class VmDebuggerLogic {
         ]);
       });
 
-      self._traceManager.getRemainingGas(index, function(error, remaining) {
+      self._traceManager.getRemainingGas(index, function(
+        error: Error,
+        remaining: any
+      ): void {
         self.event.trigger("traceRemainingGasUpdate", [error, remaining]);
       });
 
-      self._traceManager.getReturnValue(index, function(error, returnValue) {
+      self._traceManager.getReturnValue(index, function(
+        error: Error,
+        returnValue: any
+      ): void {
         if (error) {
           self.event.trigger("traceReturnValueUpdate", [[error]]);
         } else if (self.stepManager.currentStepIndex === index) {
@@ -202,27 +235,37 @@ export default class VmDebuggerLogic {
     });
   }
 
-  listenToFullStorageChanges() {
+  listenToFullStorageChanges(): void {
     const self = this;
 
     this.address = [];
     this.traceLength = 0;
 
-    self.debugger.event.register("newTraceLoaded", function(length) {
-      self._traceManager.getAddresses(function(error, addresses) {
+    self.debugger.event.register("newTraceLoaded", function(
+      length: number
+    ): void {
+      self._traceManager.getAddresses(function(
+        error: Error,
+        addresses: any
+      ): void {
         if (error) return;
         self.event.trigger("traceAddressesUpdate", [addresses]);
         self.addresses = addresses;
       });
 
-      self._traceManager.getLength(function(error, length) {
+      self._traceManager.getLength(function(
+        error: Error,
+        length: number
+      ): void {
         if (error) return;
         self.event.trigger("traceLengthUpdate", [length]);
         self.traceLength = length;
       });
     });
 
-    self.debugger.event.register("indexChanged", this, function(index) {
+    self.debugger.event.register("indexChanged", this, function(
+      index: number
+    ): void {
       if (index < 0) return;
       if (self.stepManager.currentStepIndex !== index) return;
       if (!self.storageResolver) return;
@@ -242,7 +285,7 @@ export default class VmDebuggerLogic {
           self.storageResolver,
           self._traceManager
         );
-        storageViewer.storageRange(function(error, result) {
+        storageViewer.storageRange(function(error: Error, result: any): void {
           if (!error) {
             storageJSON[address] = result;
             self.event.trigger("traceLengthUpdate", [storageJSON]);
@@ -252,39 +295,41 @@ export default class VmDebuggerLogic {
     });
   }
 
-  listenToNewChanges() {
+  listenToNewChanges(): any {
     const self = this;
-    self.debugger.event.register("newTraceLoaded", this, function() {
+    self.debugger.event.register("newTraceLoaded", this, function(): void {
       self.storageResolver = new StorageResolver({ web3: self.debugger.web3 });
       self.debuggerSolidityState.storageResolver = self.storageResolver;
       self.debuggerSolidityLocals.storageResolver = self.storageResolver;
       self.event.trigger("newTrace", []);
     });
 
-    self.debugger.event.register("callTreeReady", function() {
+    self.debugger.event.register("callTreeReady", function(): any {
       if (self.debugger.callTree.reducedTrace.length) {
         return self.event.trigger("newCallTree", []);
       }
     });
   }
 
-  listenToSolidityStateEvents() {
+  listenToSolidityStateEvents(): void {
     const self = this;
     this.event.register(
       "indexChanged",
       this.debuggerSolidityState.init.bind(this.debuggerSolidityState)
     );
-    this.debuggerSolidityState.event.register("solidityState", function(state) {
+    this.debuggerSolidityState.event.register("solidityState", function(
+      state: any
+    ): void {
       self.event.trigger("solidityState", [state]);
     });
     this.debuggerSolidityState.event.register("solidityStateMessage", function(
-      message
+      message: any
     ) {
       self.event.trigger("solidityStateMessage", [message]);
     });
     this.debuggerSolidityState.event.register(
       "solidityStateUpdating",
-      function() {
+      function(): void {
         self.event.trigger("solidityStateUpdating", []);
       }
     );
@@ -298,32 +343,32 @@ export default class VmDebuggerLogic {
     );
   }
 
-  listenToSolidityLocalsEvents() {
+  listenToSolidityLocalsEvents(): void {
     const self = this;
     this.event.register(
       "sourceLocationChanged",
       this.debuggerSolidityLocals.init.bind(this.debuggerSolidityLocals)
     );
     this.debuggerSolidityLocals.event.register("solidityLocals", function(
-      state
+      state: any
     ) {
       self.event.trigger("solidityLocals", [state]);
     });
     this.debuggerSolidityLocals.event.register(
       "solidityLocalsMessage",
-      function(message) {
+      function(message: any): void {
         self.event.trigger("solidityLocalsMessage", [message]);
       }
     );
     this.debuggerSolidityLocals.event.register(
       "solidityLocalsUpdating",
-      function() {
+      function(): void {
         self.event.trigger("solidityLocalsUpdating", []);
       }
     );
     this.debuggerSolidityLocals.event.register(
       "traceReturnValueUpdate",
-      function(data, header) {
+      function(data: any, header: any): void {
         self.event.trigger("traceReturnValueUpdate", [data, header]);
       }
     );
